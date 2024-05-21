aff33336b888ae6bb8ef1c730adbe29843f0d9ad
Erwin Mesias <mesiaserwin@gmail.com>
2024-05-21 23:46:39 +0800

    feat(eol): update text

--- src/components/common/EndOfList.tsx
+++ src/components/common/EndOfList.tsx
@@ -1,9 +1,13 @@
 function EndOfList() {
   return (
     <div className="flex items-center justify-center my-12 gap-3">
-      <hr className="border-t-2 border-t-primary w-full max-w-xs" />
-      <p className="text-primary-500 text-sm whitespace-nowrap">End of list</p>
-      <hr className="border-t-2 border-t-primary w-full max-w-xs" />
+      <hr className="border-t-2 border-t-primary w-full max-w-12" />
+      <p className="text-primary-500 text-sm whitespace-nowrap">
+        We've run out of cats to show
+        <br />
+        They've all gone for a catnap.
+      </p>
+      <hr className="border-t-2 border-t-primary w-full max-w-12" />
     </div>
   )
 }
