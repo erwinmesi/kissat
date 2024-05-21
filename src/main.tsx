bfc36b4c0a8a8a9b6757f344f336fd1350006efc
Erwin Mesias <mesiaserwin@gmail.com>
2024-05-21 16:05:18 +0800

    feat(styles): sass setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 
+import '@/assets/styles/main.scss';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
