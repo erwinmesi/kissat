2201bf3a6f88c1e13ee1e74cf7c3bd7be694f887
Erwin Mesias <mesiaserwin@gmail.com>
2024-05-21 16:47:37 +0800

    build: add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,17 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+
+    // Enforce the use of single quotes
+    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
+
+    // Enforce no semicolons at the end of lines
+    'semi': ['error', 'never'],
+
+    // Allow the use of any type
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // Allow empty dependencies array in hooks
+    'react-hooks/exhaustive-deps': ['off', 'never'],
   },
 }
